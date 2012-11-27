42ffa452ae44a4e8a1c8b9956082987f3f4b597e
iTidez <iTidez@diamondcraft.co>
2012-11-26 23:03:55 -0500

    Added ChatManager to handle chat messaging

--- plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/chat/ChatManager.java
+++ plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/chat/ChatManager.java
@@ -4,10 +4,29 @@
  */
 package me.itidez.plugins.iminettt.chat;
 
+import me.itidez.plugins.iminettt.Iminettt;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
 /**
  *
  * @author tjs238
  */
-public class ChatManager {
+public class ChatManager implements Listener{
+    private Iminettt plugin;
     
+    public ChatManager(Iminettt plugin) {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler (priority = EventPriority.MONITOR)
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String playerName = player.getDisplayName();
+        
+        
+    }
 }
