688661ff0fb5c18c18f1b969ef32951fca5871cd
iTidez <iTidez@diamondcraft.co>
2012-11-29 21:05:30 -0500

    Added Map ENUM and Map class

---
+++ plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/game/Map.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.itidez.plugins.iminettt.game;
+
+/**
+ *
+ * @author tjs238
+ */
+public enum Map {
+    AFGHAN(0) {
+        @Override
+        public Class getMap() {
+            return AfghanMap.class;
+        }
+    },
+    RAND(1) {
+        @Override
+        public Class getMap() {
+            return WorldManager.getRandMap();
+        }
+    };
+    
+    private Class css;
+    private int mapId;
+    public abstract Class getMap();
+    
+    private Map(int mapId) {
+        this.mapId = mapId;
+    }
+}
