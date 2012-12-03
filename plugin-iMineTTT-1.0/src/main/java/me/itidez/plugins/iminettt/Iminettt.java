7d4e5f178481358388e276b27b83357ae888a96f
iTidez <iTidez@diamondcraft.co>
2012-12-03 14:52:11 -0500

    Changed Vault methods to VaultManager class

--- plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/Iminettt.java
+++ plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/Iminettt.java
@@ -2,9 +2,9 @@ package me.itidez.plugins.iminettt;
 
 import java.util.logging.Logger;
 import me.itidez.plugins.iminettt.commands.AdminCommand;
-import net.milkbowl.vault.chat.Chat;
+import me.itidez.plugins.iminettt.commands.DonorShopCommand;
+import me.itidez.plugins.iminettt.commands.ShopCommand;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Iminettt extends JavaPlugin {
@@ -12,7 +12,6 @@ public class Iminettt extends JavaPlugin {
     private PluginDescriptionFile description;
     private String prefix;
     private String version;
-    public static Chat chat = null;
     public boolean debug = false;
     
     @Override
@@ -26,19 +25,20 @@ public class Iminettt extends JavaPlugin {
         this.description = getDescription();
         this.version = this.description.getVersion();
         this.prefix = ("[" + this.description.getName() + "] ");
-        setupChat();
         debug("Setting up error logger");
         ErrorLogger.register(this, "iMineTTT", "me.itidez.plugins", "http://mantis.derpcraft.co/");
         debug("Registered Error Logger");
         CommandManager commandRegistrator = new CommandManager(this);
         commandRegistrator.register(AdminCommand.class);
         commandRegistrator.register(ShopCommand.class); // iTidez Edit: Added on GitHub (Fetch REPO)
+        commandRegistrator.register(DonorShopCommand.class);
         debug("Registered Commands"); // iTidez Edit: Added on GitHub (Fetch REPO)
         /*
          * Remote Entity Manager Setup
          * Do not edit past class file name
          */
-        EntityManager manager = RemoteEntities.createManager(this);
+        //EntityManager manager = RemoteEntities.createManager(this);
+        
         // Actual Entity variable. Assign player death's location to spawn location
         // RemoteEntity entity = manager.createEntity(RemoteEntity.Zombie,     Bukkit.getWorld("world").getSpawnLocation(), false);
         
@@ -50,16 +50,6 @@ public class Iminettt extends JavaPlugin {
     
     public void debug(String message) {
         this.log.info("[DEV]"+this.prefix+message);
-    }
-    
-    private boolean setupChat()
-    {
-        RegisteredServiceProvider chatProvider = getServer().getServicesManager().getRegistration(Chat.class);
-        if (chatProvider != null) {
-            chat = (Chat)chatProvider.getProvider();
-            log("Vault Loaded!");
-        }
-        return chat != null;
     }
 }
 
