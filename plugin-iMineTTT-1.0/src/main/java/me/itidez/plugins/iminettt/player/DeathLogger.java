c6c37526b02e24d27e36e47be741f1b9deea3fca
iTidez <iTidez@diamondcraft.co>
2012-12-03 14:53:36 -0500

    Modified to include Villager Entity detection based on rank

---
+++ plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/player/DeathLogger.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.itidez.plugins.iminettt.player;
+
+import de.kumpelblase2.remoteentities.EntityManager;
+import de.kumpelblase2.remoteentities.RemoteEntities;
+import de.kumpelblase2.remoteentities.api.RemoteEntity;
+import de.kumpelblase2.remoteentities.api.RemoteEntityType;
+import de.kumpelblase2.remoteentities.exceptions.NoNameException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import me.itidez.plugins.iminettt.Iminettt;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import de.kumpelblase2.remoteentities.api.*;
+import de.kumpelblase2.remoteentities.api.thinking.InteractBehavior;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+/**
+ *
+ * @author tjs238
+ */
+public class DeathLogger implements Listener{
+    private Iminettt plugin;
+    private EntityManager manager;
+    
+    public DeathLogger(Iminettt plugin) {
+        this.plugin = plugin;
+        this.manager = RemoteEntities.createManager(plugin);
+    }
+    
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        final Player player = event.getEntity();
+        if(player instanceof Player) {
+            RemoteEntity entity;
+            try {
+                entity = manager.createEntity(RemoteEntityType.Villager, player.getLocation(), false);
+                entity.setStationary(true);
+                entity.getMind().addBehaviour(new InteractBehavior(entity) {
+                    @Override
+                    public void onInteract(Player inPlayer)
+                    {
+                        if(inPlayer.getMetadata("Team").toString().equalsIgnoreCase("Detective")) {
+                            inPlayer.sendMessage(ChatColor.YELLOW+"You have found the body of "+player.getName()+", they were a(n) "+player.getMetadata("Team").toString());
+                            Bukkit.broadcastMessage(ChatColor.GOLD+"The body of "+player.getName()+" has been found! They were a(n) "+player.getMetadata("Team").toString());
+                        } else {
+                           
+                        }
+                    }
+                });
+            } catch (NoNameException ex) {
+                Logger.getLogger(DeathLogger.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
