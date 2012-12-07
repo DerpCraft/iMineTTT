beb826679afa16fe9f1a06e69b6ea4857c227167
iTidez <itidez@diamondcraft.co>
2012-12-07 06:49:47 -0800

    Create Warp.java

---
+++ plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/Warp.java
@@ -0,0 +1,13 @@
+package me.itidez.plugins.iminettt;
+
+
+public class Warp {
+	private String name;
+	private Player owner;
+	private Player[] members;
+	private Location warpLoc;
+	
+	public Warp(String name) {
+
+	}
+}
