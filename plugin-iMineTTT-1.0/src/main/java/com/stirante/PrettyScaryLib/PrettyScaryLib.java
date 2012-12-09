44731f8c43750f088c7914e04f11d0a00344bfb3
iTidez <iTidez@diamondcraft.co>
2012-12-09 09:58:14 -0500

    Added New Library

---
+++ plugin-iMineTTT-1.0/src/main/java/com/stirante/PrettyScaryLib/PrettyScaryLib.java
@@ -0,0 +1,29 @@
+package com.stirante.PrettyScaryLib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The Class PrettyScaryLib.
+ */
+public class PrettyScaryLib extends JavaPlugin {
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.bukkit.plugin.java.JavaPlugin#onEnable()
+	 */
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.bukkit.plugin.java.JavaPlugin#onDisable()
+	 */
+	@Override
+	public void onDisable() {
+		
+	}
+}
---
+++ plugin-iMineTTT-1.0/src/main/java/com/stirante/PrettyScaryLib/Skull.java
@@ -0,0 +1,180 @@
+package com.stirante.PrettyScaryLib;
+
+import java.lang.reflect.Field;
+
+import net.minecraft.server.NBTTagCompound;
+import net.minecraft.server.TileEntitySkull;
+
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.CraftWorld;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Class, that allows setting and getting skin fo skull.
+ */
+public class Skull {
+	
+	/**
+	 * Sets skin.
+	 * 
+	 * @param item
+	 *            item
+	 * @param nick
+	 *            nick
+	 * @return item stack
+	 */
+	public static ItemStack setSkin(ItemStack item, String nick) {
+		if (!isApplicable(item)) return null;
+		CraftItemStack craftStack = null;
+		net.minecraft.server.ItemStack itemStack = null;
+		if (item instanceof CraftItemStack) {
+			craftStack = (CraftItemStack) item;
+			itemStack = craftStack.getHandle();
+		}
+		else if (item instanceof ItemStack) {
+			craftStack = new CraftItemStack(item);
+			itemStack = craftStack.getHandle();
+		}
+		NBTTagCompound tag = itemStack.tag;
+		if (tag == null) tag = new NBTTagCompound();
+		tag.setString("SkullOwner", nick);
+		itemStack.tag = tag;
+		return craftStack;
+	}
+	
+	/**
+	 * Sets skin.
+	 * 
+	 * @param block
+	 *            block
+	 * @param nick
+	 *            nick
+	 */
+	public static void setSkin(Block block, String nick) {
+		if (!isApplicable(block)) return;
+		TileEntitySkull skull = (TileEntitySkull) ((CraftWorld) block
+				.getWorld()).getHandle().getTileEntity(block.getX(),
+				block.getY(), block.getZ());
+		skull.setSkullType(skull.getSkullType(), nick);
+	}
+	
+	/**
+	 * Gets rotation.
+	 * 
+	 * @param block
+	 *            block
+	 * @param rotation
+	 *            rotation
+	 * @return rotation
+	 */
+	public static int getRotation(Block block, int rotation) {
+		if (!isApplicable(block)) return -1;
+		TileEntitySkull skull = (TileEntitySkull) ((CraftWorld) block
+				.getWorld()).getHandle().getTileEntity(block.getX(),
+				block.getY(), block.getZ());
+		return skull.p & 7;
+	}
+	
+	/**
+	 * Sets rotation.
+	 * 
+	 * @param block
+	 *            block
+	 * @param rotation
+	 *            rotation
+	 */
+	public static void setRotation(Block block, int rotation) {
+		if (!isApplicable(block)) return;
+		TileEntitySkull skull = (TileEntitySkull) ((CraftWorld) block
+				.getWorld()).getHandle().getTileEntity(block.getX(),
+				block.getY(), block.getZ());
+		skull.setRotation(rotation);
+		
+	}
+	
+	/**
+	 * Gets skin.
+	 * 
+	 * @param block
+	 *            block
+	 * @param nick
+	 *            nick
+	 * @return nick
+	 */
+	public static String getSkin(Block block) {
+		if (!isApplicable(block)) return null;
+		TileEntitySkull skull = (TileEntitySkull) ((CraftWorld) block
+				.getWorld()).getHandle().getTileEntity(block.getX(),
+				block.getY(), block.getZ());
+		try {
+			Field field = TileEntitySkull.class.getDeclaredField("c");
+			field.setAccessible(true);
+			return (String) field.get(skull);
+		}
+		catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+		catch (SecurityException e) {
+			e.printStackTrace();
+		}
+		catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Gets skin.
+	 * 
+	 * @param item
+	 *            item
+	 * @return skin
+	 */
+	public static String getSkin(ItemStack item) {
+		if (!isApplicable(item)) return null;
+		CraftItemStack craftStack = null;
+		net.minecraft.server.ItemStack itemStack = null;
+		if (item instanceof CraftItemStack) {
+			craftStack = (CraftItemStack) item;
+			itemStack = craftStack.getHandle();
+		}
+		else if (item instanceof ItemStack) {
+			craftStack = new CraftItemStack(item);
+			itemStack = craftStack.getHandle();
+		}
+		NBTTagCompound tag = itemStack.tag;
+		if (tag == null) {
+			tag = new NBTTagCompound();
+			return null;
+		}
+		return tag.getString("SkullOwner");
+	}
+	
+	/**
+	 * Checks if is applicable.
+	 * 
+	 * @param item
+	 *            item
+	 * @return true, if is applicable
+	 */
+	public static boolean isApplicable(Object obj) {
+		if (obj instanceof ItemStack)
+			switch (((ItemStack) obj).getType()) {
+				case SKULL_ITEM:
+					return true;
+				default:
+					return false;
+			}
+		else if (obj instanceof Block) switch (((Block) obj).getType()) {
+			case SKULL:
+				return true;
+			default:
+				return false;
+		}
+		return false;
+	}
+}
