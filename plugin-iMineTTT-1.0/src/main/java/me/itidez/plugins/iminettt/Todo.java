d0b83c0d0a07bbb2127347e364aa6fd14e5ed06a
iTidez <itidez@diamondcraft.co>
2013-01-08 09:48:36 -0500

    Update plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/Todo.java

--- plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/Todo.java
+++ plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/Todo.java
@@ -4,5 +4,27 @@
 package me.itidez.plugins.iminettt;
 
 public class Todo {
-
+	
+	private List todo = new ArrayList();
+	
+	public Todo() {
+		
+	}
+	
+	public List getList() {
+		return todo;
+	}
+	
+	public void addToList(String addition) {
+		todo.add(addition);
+	}
+	
+	public boolean removeFromList(String rem) {
+		if(todo.contains(rem)) {
+			todo.remove(rem);
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
