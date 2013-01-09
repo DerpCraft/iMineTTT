b06bd38a2845c2f9ea8e9b01b5eeddf7b9e3bbbd
iTidez <itidez@diamondcraft.co>
2013-01-09 07:18:30 -0800

    Create WorldManager.java

---
+++ plugin-iMineTTT-1.0/src/main/java/me/itidez/plugins/iminettt/chat/WorldManager.java
@@ -0,0 +1,40 @@
+package me.itidez.plugins.iminettt.chat;
+
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import com.onarandombox.MultiverseCore.api.MVWorldManager;
+import org.bukkit.plugin.Plugin;
+
+/**
+*
+* @author Krin
+*/
+public class WorldManager {
+    private ChatCore plugin;
+    private MVWorldManager MVWorldManager;
+
+    public WorldManager(ChatCore instance) {
+        plugin = instance;
+        fetchAliases();
+    }
+
+    public void clean() {
+        MVWorldManager = null;
+    }
+
+    public String getAlias(String world) {
+        if (MVWorldManager != null && MVWorldManager.isMVWorld(world)) {
+            return MVWorldManager.getMVWorld(world).getColoredWorldString();
+        }
+        return world;
+    }
+
+    private void fetchAliases() {
+        Plugin tmp = plugin.getServer().getPluginManager().getPlugin("Multiverse-Core");
+        if (tmp != null) {
+            plugin.debug("Found Multiverse-Core! Registering aliases...");
+            MultiverseCore multiverse = (MultiverseCore) tmp;
+            MVWorldManager = multiverse.getMVWorldManager();
+        }
+    }
+
+}
